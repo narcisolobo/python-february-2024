b0f9018a78f21382f716707fa355c304795876c4
narcisolobo <narcisolobo@gmail.com>
2024-03-05 17:42:26 -0800

    added sql script

---
+++ w3.01-sql-queries/w3-sql-queries-02-crud/crud.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES
+("Etta", "James", "etta@atlast.com", "atlast"),
+("Bill", "Withers", "bill@stillbill.com", "aintnosunshine"),
+("Gladys", "Knight", "gladys@thepips.com", "midnighttrain");
