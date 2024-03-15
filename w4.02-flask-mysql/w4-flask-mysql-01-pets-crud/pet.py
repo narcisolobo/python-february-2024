dd8fe257373e9b1f1ae40cf98da893ae805638a1
narcisolobo <narcisolobo@gmail.com>
2024-03-14 18:20:40 -0700

    w4d3 code

--- w4.02-flask-mysql/w4-flask-mysql-01-pets-crud/pet.py
+++ w4.02-flask-mysql/w4-flask-mysql-01-pets-crud/pet.py
@@ -3,6 +3,8 @@ from pprint import pprint
 
 
 class Pet:
+    _db = "pets_db"
+
     # this constructor accepts a dictionary as input
     def __init__(self, data):
         self.id = data["id"]
@@ -22,7 +24,7 @@ class Pet:
         """Find all pets in the database."""
 
         query = "SELECT * FROM pets;"
-        list_of_dicts = connectToMySQL("pets_db").query_db(query)
+        list_of_dicts = connectToMySQL(Pet._db).query_db(query)
         pprint(list_of_dicts)
 
         pets = []
@@ -41,5 +43,46 @@ class Pet:
         VALUES
         (%(name)s, %(type)s, %(birthday)s, %(is_derpy)s);
         """
-        pet_id = connectToMySQL("pets_db").query_db(query, form_data)
+        pet_id = connectToMySQL(Pet._db).query_db(query, form_data)
         return pet_id
+
+    @classmethod
+    def find_by_id(cls, pet_id):
+        """Finds one pet by id in the database."""
+
+        query = "SELECT * FROM pets WHERE id = %(pet_id)s;"
+        data = {"pet_id": pet_id}
+        list_of_dicts = connectToMySQL(Pet._db).query_db(query, data)
+        print("***************************")
+        pprint(list_of_dicts)
+        print("***************************")
+        # error handling
+        if len(list_of_dicts) == 0:
+            return None
+        pet = Pet(list_of_dicts[0])
+        return pet
+
+    @classmethod
+    def update(cls, form_data):
+        """Updates a pet from a form."""
+
+        query = """
+        UPDATE pets
+        SET
+        name=%(name)s,
+        type=%(type)s,
+        birthday=%(birthday)s,
+        is_derpy=%(is_derpy)s
+        WHERE id = %(pet_id)s;
+        """
+        connectToMySQL(Pet._db).query_db(query, form_data)
+        return
+
+    @classmethod
+    def delete_by_id(cls, pet_id):
+        """Deletes a pet by id."""
+
+        query = "DELETE FROM pets WHERE id = %(pet_id)s;"
+        data = {"pet_id": pet_id}
+        connectToMySQL(Pet._db).query_db(query, data)
+        return
--- w4.02-flask-mysql/w4-flask-mysql-01-pets-crud/server.py
+++ w4.02-flask-mysql/w4-flask-mysql-01-pets-crud/server.py
@@ -13,7 +13,7 @@ def index():
     return render_template("index.html", pets=pets)
 
 
-@app.route("/pets/new")
+@app.get("/pets/new")
 def new_pet():
     """This route displays the new pet form."""
 
@@ -24,9 +24,49 @@ def new_pet():
 # @app.route("/pets/create", methods=["POST"])
 def create_pet():
     """The route that processes the form."""
+
     print(request.form)
     pet_id = Pet.create(request.form)
     print("THIS IS THE NEW PET'S ID: " + str(pet_id))
+    return redirect("/pets")
+
+
+@app.get("/pets/<int:pet_id>")
+def pet_details(pet_id):
+    """This route displays one pet's details."""
+
+    pet = Pet.find_by_id(pet_id)
+    if pet == None:
+        return "Cannot find pet."
+
+    print(type(pet))
+    return render_template("pet_details.html", pet=pet)
+
+
+@app.get("/pets/<int:pet_id>/edit")
+def edit_pet(pet_id):
+    """This route displays the edit form."""
+
+    pet = Pet.find_by_id(pet_id)
+    if pet == None:
+        return "Cannot find pet."
+    return render_template("edit_pet.html", pet=pet)
+
+
+@app.post("/pets/update")
+def update_pet():
+    """This route processes the edit form."""
+
+    pet_id = request.form["pet_id"]
+    Pet.update(request.form)
+    return redirect(f"/pets/{pet_id}")
+
+
+@app.post("/pets/<int:pet_id>/delete")
+def delete_pet(pet_id):
+    """This route deletes a pet."""
+
+    Pet.delete_by_id(pet_id)
     return redirect("/pets")
 
 
