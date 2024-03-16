7cd7266bd7f2c173faf31d5daff972fd2f29fa10
narcisolobo <narcisolobo@gmail.com>
2024-03-15 20:42:57 -0700

    updated starters

--- w5.01-relationships/starters/server.py
+++
@@ -1,7 +0,0 @@
-from flask_app import app
-
-# import controllers here
-# from flask_app.controllers import gizmos
-
-if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port=5500)
